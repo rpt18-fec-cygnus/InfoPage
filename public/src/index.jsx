1aa4fbafca66cad085f86120381bbb13087519ea
Takanori Sono <takanori.sono@gmail.com>
2020-02-08 12:34:50 -0800

    Set up endpoints for proxy and render to DOM node infoPage

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/Button.jsx'
-import mendocinoFarms from '../../mockData/createData.js'
+// import mendocinoFarms from '../../mockData/createData.js'
 import MainInfo from './components/MainInfo.jsx'
 import axios from 'axios';
 
@@ -14,19 +14,29 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // var url = document.URL;
+    // console.log(window.location.pathname)
+    //split url by key restaurant
+    //if split url length is 1 (didn't split) then get restaurant id 1
+    if (window.location.pathname === '/') {
+      var endpoint = '/restaurant/1'
+    } else {
+      var endpoint = window.location.pathname;
+    }
+    
     //get request using axios for restaurant info once component mounts
     // axios.get('/restaurantInfo')
 
     // const urlParams = new URLSearchParams(this.props.location.search);
     // console.log('this is urlParams: ', urlParams)
     
-    axios.get('/restaurant/1')
+    axios.get(`http://localhost:7000${endpoint}`)
       .then((res) => {
         this.setState(res.data)
         return res.data
       })
       .catch(err => console.log(err))
-      .then((data) => console.log(data.keyDesc))
+      // .then((data) => console.log(data.keyDesc))
   }
 
   render() {
@@ -48,4 +58,5 @@ class App extends React.Component {
   }  
 }
 
-ReactDOM.render( <App sampleData={mendocinoFarms}/>, document.getElementById('root'));
+// ReactDOM.render( <App sampleData={mendocinoFarms}/>, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('infoPage'));
--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 7000;
-const {getRestaurantByID} = require('../mockData/dbHelper.js')
+const proxy = require('http-proxy-middleware')
+const {getRestaurantByID} = require('../mockData/dbHelper.js');
+const cors = require('cors');
+
+app.use(cors());
+
 
 app.use(express.static('public'));
+// app.use(
+//   '/restaurant',
+//   proxy({target: 'http://localhost:7000'})
+// )
 
-app.get('/restaurantInfo', (req, res) => {
-  // console.log(getRestaurantByID)
-  getRestaurantByID(1, (data) => {
-    res.send(data);
-  })
-  // res.send('fake data');
-})
+// app.get('/restaurantInfo', (req, res) => {
+//   getRestaurantByID(1, (data) => {
+//     res.send(data);
+//   })
+// })
 
 app.get('/restaurant/:restaurantId', (req, res) => {
-  //req.params key value pair of restaurantID
-
-  
-  // res.send(req.params.restaurantId)
+  console.log(`this is req.params: ${req.params.restaurantId}`)
   getRestaurantByID(req.params.restaurantId, (data) => {
     res.send(data);
-  })
-  // res.send('fake data');
+  });
 })
 
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
