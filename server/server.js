d86ab8529b1efb4083535c35e81d8e0ae89ad639
Takanori Sono <takanori.sono@gmail.com>
2020-02-18 19:37:25 -0800

    Create and add API endpoint to render by /restaurantName

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 7000;
 const proxy = require('http-proxy-middleware')
-const {getRestaurantByID} = require('../mockData/dbHelper.js');
+const {getRestaurantByID, getRestaurantByName} = require('../mockData/dbHelper.js');
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -13,12 +13,20 @@ app.use(cors());
 app.use('/', express.static('public'));
 
 //toggle for proxy versus service
-app.use('/restaurant/:restaurantId', express.static('public'))
+app.use('/restaurant/:restaurantId', express.static('public'));
+app.use('/restaurantName/:restaurantName', express.static('public'));
+
 app.get('/api/restaurant/:restaurantId', (req, res) => {
   // console.log(`this is req.params: ${req.params.restaurantId}`)
   getRestaurantByID(req.params.restaurantId, (data) => {
     res.send(data);
   });
 });
-
+app.get('/api/restaurantName/:restaurantName', (req, res) => {
+  // console.log(`this is req.params: ${req.params.restaurantId}`)
+  // res.send('endpoint works')
+  getRestaurantByName(req.params.restaurantName, (data) => {
+    res.send(data);
+  });
+});
 app.listen(port, () => console.log(`Cygnus-Yelp App Listening on port ${port}!`));
