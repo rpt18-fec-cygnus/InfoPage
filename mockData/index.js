907618a3bc3cbd6edf6f2845872aa8f08983ab44
Takanori Sono <takanori.sono@gmail.com>
2020-02-26 19:42:26 -0800

    Migrate data to mongodb Atlasand code ready for deploy

--- mockData/index.js
+++ mockData/index.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/mockRestaurants', { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connect('mongodb://localhost/mockRestaurants', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connect('mongodb+srv://dbUser:ulmscxCrdbphfO7F@mockrestaurants-9c1d0.mongodb.net/mockRestaurants?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {console.log(err)};
+  console.log('connected to mongoDB Atlas database!')
+});
 
 module.exports = db;
 
